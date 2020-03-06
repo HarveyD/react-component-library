0e9e5f9622d94e6984e35d3964b3b19b7c0da7dc
Harvey Delaney <harvey@harveydelaney.com>
2020-03-06 21:42:05 +1100

    fix: update package.json and rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,31 @@
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "rollup-plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
 import sass from "rollup-plugin-sass";
 import commonjs from "rollup-plugin-commonjs";
-import external from "rollup-plugin-peer-deps-external";
-import resolve from "rollup-plugin-node-resolve";
 
-import pkg from "./package.json";
+import packageJson from "./package.json";
 
 export default {
   input: "src/index.tsx",
   output: [
     {
-      file: pkg.main,
+      file: packageJson.main,
       format: "cjs",
-      exports: "named",
       sourcemap: true
     },
     {
-      file: pkg.module,
-      format: "es",
-      exports: "named",
+      file: packageJson.module,
+      format: "esm",
       sourcemap: true
     }
   ],
   plugins: [
-    external(),
+    peerDepsExternal(),
     resolve({
       browser: true
     }),
-    typescript({
-      rollupCommonJSResolveHack: true,
-      exclude: "**/__tests__/**",
-      clean: true
-    }),
+    typescript(),
     commonjs({
       include: ["node_modules/**"],
       exclude: ["**/*.stories.js"],
