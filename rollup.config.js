27cade06eb9d1be615b6a571a5e9aac815bcd2bc
Harvey Delaney <harvey@harveydelaney.com>
2020-06-21 21:09:25 +1000

    refactor: use postcss instead of rollup-sass

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
-import sass from "rollup-plugin-sass";
+import postcss from "rollup-plugin-postcss";
 import copy from "rollup-plugin-copy";
 
 const packageJson = require("./package.json");
@@ -26,9 +26,7 @@ export default {
     resolve(),
     commonjs(),
     typescript({ useTsconfigDeclarationDir: true }),
-    sass({
-      insert: true
-    }),
+    postcss(),
     copy({
       targets: [
         {
