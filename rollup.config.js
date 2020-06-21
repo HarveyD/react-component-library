fab2a2ca9a17bdf8cd1fe363e02eebc56189173b
Harvey Delaney <harvey@harveydelaney.com>
2020-06-21 21:32:06 +1000

    feat: use styled-components instead of sass

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,6 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
-import postcss from "rollup-plugin-postcss";
-import copy from "rollup-plugin-copy";
 
 const packageJson = require("./package.json");
 
@@ -21,25 +19,11 @@ export default {
       sourcemap: true
     }
   ],
+  external: ["styled-components"],
   plugins: [
     peerDepsExternal(),
     resolve(),
     commonjs(),
-    typescript({ useTsconfigDeclarationDir: true }),
-    postcss(),
-    copy({
-      targets: [
-        {
-          src: "src/variables.scss",
-          dest: "build",
-          rename: "variables.scss"
-        },
-        {
-          src: "src/typography.scss",
-          dest: "build",
-          rename: "typography.scss"
-        }
-      ]
-    })
+    typescript({ useTsconfigDeclarationDir: true })
   ]
 };
--- src/TestComponent/TestComponent.tsx
+++ src/TestComponent/TestComponent.tsx
@@ -1,17 +1,32 @@
 import React from "react";
+import styled from "styled-components";
 
 import { TestComponentProps } from "./TestComponent.types";
 
-import "./TestComponent.scss";
+const StyledDiv = styled.div`
+  background-color: white;
+  border: 1px solid black;
+  padding: 16px;
+  width: 360px;
+  text-align: center;
+
+  ${(props) =>
+    props.theme === "secondary" &&
+    `background-color: black;
+     color: white;`}
+`;
+
+const StyledHeading = styled.h1`
+  font-size: 32px;
+`;
+
+const StyledDescription = styled.h2``;
 
 const TestComponent: React.FC<TestComponentProps> = ({ theme }) => (
-  <div
-    data-testid="test-component"
-    className={`test-component test-component-${theme}`}
-  >
-    <h1 className="heading">I'm the test component</h1>
-    <h2>Made with love by Harvey</h2>
-  </div>
+  <StyledDiv data-testid="test-component" theme={theme}>
+    <StyledHeading className="heading">I'm the test component</StyledHeading>
+    <StyledDescription>Made with love by Harvey</StyledDescription>
+  </StyledDiv>
 );
 
 export default TestComponent;
