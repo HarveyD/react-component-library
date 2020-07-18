7f0785cbd0c7b204a1be7ace10987bff17a3d077
Harvey Delaney <harvey@harveydelaney.com>
2020-07-18 15:16:07 +1000

    feat: use Webpack to transpile/bundle component library

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,25 +1,14 @@
-const path = require("path");
+const custom = require("../webpack.config.js");
 
 module.exports = {
   stories: ["../src/**/*.stories.tsx"],
   // Add any Storybook addons you want here: https://storybook.js.org/addons/
   addons: [],
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: ["style-loader", "css-loader", "sass-loader"],
-      include: path.resolve(__dirname, "../")
-    });
-
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      loader: require.resolve("babel-loader"),
-      options: {
-        presets: [["react-app", { flow: false, typescript: true }]]
-      }
-    });
+  webpackFinal: (config) => {
     config.resolve.extensions.push(".ts", ".tsx");
-
-    return config;
+    return {
+      ...config,
+      module: { ...config.module, rules: custom.module.rules }
+    };
   }
 };
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].js",
+    library: "react-component-library",
+    libraryTarget: "umd"
+  },
+  resolve: {
+    extensions: [".ts", ".tsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: [
+          {
+            loader: require.resolve("ts-loader")
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+        include: path.resolve(__dirname, "./src")
+      }
+    ]
+  },
+  externals: {
+    react: "react",
+    "react-dom": "react-dom"
+  }
+};
